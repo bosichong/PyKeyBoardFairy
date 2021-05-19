6c626607d2bd52dbf1f15202ba472390b02738e0
J.sky <bosichong@qq.com>
2021-05-19 10:40:50 +0800

    完成预订的基本功能

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ Mail    : bosichong@qq.com
 
 
 __version__ = "0.1.0"
-from pynput.keyboard import Key, Controller, Listener
+from pynput.keyboard import Key,KeyCode, Controller, Listener
 import threading
 import time
 
 
 '''
 按键配置说明：
-"key_type": "interval",#按键种类：
+"key_type": "interval",
+
+按键种类：
 interval：魔法辅助技能键，间隔一定时间按一次
 always：一直按着不放开的键，中间有少量的间暂停
+combination：组合技能，一组按键按照一定顺序和间隔时间的模拟按下
+
 "key_switch": Key.ctrl,#开关控制键，负责控制模拟这个技能键的开关
 "key": 'b',#模拟的技能键
 "is_start": 0,#开关，确定当前技能键在程序开启时，默认是关闭的，一般为0及可
@@ -31,37 +35,44 @@ always：一直按着不放开的键，中间有少量的间暂停
 keyList = [
     {
         "key_type": "interval",
-        "key_switch": Key.ctrl,
+        "key_switch": KeyCode.from_char('z'),
         "key": 'b',
         "is_start": 0,
         "t": 0.5,
     }, {
         "key_type": "always",
-        "key_switch": Key.ctrl,
+        "key_switch": KeyCode.from_char('z'),
         "key": 'c',
         "is_start": 0,
         "t": 0.5,
         "t1":5
+    }, {
+        "key_type": "combination",
+        "key_switch": Key.alt,
+        "key": 'd',
+        "is_start": 0,
+        "t": 0.1,
+        
+    }, {
+        "key_type": "combination",
+        "key_switch": Key.alt,
+        "key": 'f',
+        "is_start": 0,
+        "t": 1,
+        
+    },{
+        "key_type": "combination",
+        "key_switch": Key.alt,
+        "key": 'e',
+        "is_start": 0,
+        "t": 2,
+        
     },
 
 ]
 
 
-key_b = {
-    "key": 'b',
-    "is_start": 0,
-    "t": 0.5,
-}
-key_c = {
-    "key": 'c',
-    "is_start": 0,
-    "t": 0.5,
-}
-key_d = {
-    "key": 'd',
-    "is_start": 0,
-    "t": 0.5,
-}
+
 
 keyboard = Controller()
 
@@ -70,12 +81,15 @@ def on_press(key):
     ''''
     当开关键被按下，暂停或启动模拟按键
     '''
+    # print(key)
     for k in keyList:
+        # print(type(key),type(k["key_switch"]))
         if key == k["key_switch"]:
             if k["is_start"] == 0:
                 k["is_start"] = 1
             else:
                 k["is_start"] = 0
+        # print(k["is_start"])
 
 def on_release(key):
     pass
@@ -98,13 +112,23 @@ def pressKeyAlways(key):
             keyboard.release(key["key"])
         time.sleep(key["t"])
         # print(keys["is_start"])
+def pressKeyCombination(key):
+    '''
+    组合技能，一组按键按照一定顺序和间隔时间的模拟按下
+    '''
+    while True:
+        if key["is_start"]:
+            time.sleep(key["t"])
+            keyboard.press(key["key"])
+            keyboard.release(key["key"])
+            key["is_start"] = 0
+
 
 
 def createKeyThread(keylist):
     '''
     根据配置文件，创建所有的按键线程。
     '''
-
     for key in keylist:
         if key["key_type"] == "interval":
             # print(key["key_type"])
@@ -112,13 +136,18 @@ def createKeyThread(keylist):
             t.start()
         elif key["key_type"] == "always":
             # print(key["key_type"])
-            t1 = threading.Thread(target=pressKeyAlways, args=(key,))
-            t1.start()
+            t = threading.Thread(target=pressKeyAlways, args=(key,))
+            t.start()
+        elif key["key_type"] == "combination":
+            # print(key["key_type"])
+            t = threading.Thread(target=pressKeyCombination, args=(key,))
+            t.start()
         else:
             raise "没有这种类型的按键，请检查并重新设置按键的类型！"
 
 
 def main():
+    print(KeyCode.from_char('a'))
     createKeyThread(keyList)
 
     with Listener(on_press=on_press, on_release=on_release) as listener:
